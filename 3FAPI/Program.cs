ab74bfb77fa95c5335c2249f2617529cf85b4141
Joel Hellberg <118288883+JoelHellberg95@users.noreply.github.com>
2025-04-28 17:15:31 +0200

    init

---
+++ 3FAPI/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers(); // <-- Make sure you map controllers
+
+app.Run();
